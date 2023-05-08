34c67c03f4b1f31183ec3e4d895ba7214be484e6
unknown <bhotnikhil25@gmail.com>
2023-05-08 16:27:47 +0530

    [Nikhil]-Check Employee is Present or Absent

--- EmployeeWageProblem/EmployeeWageProblem/Program.cs
+++ EmployeeWageProblem/EmployeeWageProblem/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
+            //UC-01-  Check Employee is Present or Absent
             Console.WriteLine("Wel-Come To Employee Wage Problem");
+            int IS_FULL_TIME = 1; 
+            Random random= new Random();
+
+            //Computation
+
+            int empCheck =random.Next(0, 2);
+
+            if (empCheck ==IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
         }
     }
 }
