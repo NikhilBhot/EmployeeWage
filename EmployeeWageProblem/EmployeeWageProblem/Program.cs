eb9e908cb4babdc69743d98125b6333276aaf493
unknown <bhotnikhil25@gmail.com>
2023-05-08 16:35:37 +0530

    [Nikhil]-Calculate Daily Employee Wage

--- EmployeeWageProblem/EmployeeWageProblem/Program.cs
+++ EmployeeWageProblem/EmployeeWageProblem/Program.cs
@@ -5,22 +5,32 @@
         static void Main(string[] args)
         {
             //UC-01-  Check Employee is Present or Absent
+            //UC-02-Calculate Daily Employee Wage
             Console.WriteLine("Wel-Come To Employee Wage Problem");
-            int IS_FULL_TIME = 1; 
-            Random random= new Random();
+            //Constants
+            int IS_FULL_TIME=1;
+            int EMP_RATE_PER_HOUR = 20;
 
+            //Variables
+            int empHrs=0;
+            int empWage=0;
+
+            Random random = new Random(); 
             //Computation
 
-            int empCheck =random.Next(0, 2);
+            int empCheck = random.Next(0, 2);
 
-            if (empCheck ==IS_FULL_TIME)
+            if (empCheck == IS_FULL_TIME)
+
             {
-                Console.WriteLine("Employee is Present");
+                empHrs=8;
             }
             else
             {
-                Console.WriteLine("Employee is Absent");
+                empHrs = 0;
             }
+            empWage = empHrs *EMP_RATE_PER_HOUR;
+            Console.WriteLine("Emp Wage : "+ empWage);
         }
     }
 }
